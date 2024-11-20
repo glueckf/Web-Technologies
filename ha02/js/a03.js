c1ea748cfac09824a927ea75802b0017e158e3f3
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-20 18:02:15 +0100

    Solve a03.js

--- ha02/js/a03.js
+++ ha02/js/a03.js
@@ -5,17 +5,42 @@ let object = {
 function addObjectElement(key, value) {
   let objectCopy = Object.assign({}, object)
   // TODO: implement me
+
+  let newKey = key;
+
+  while(newKey in objectCopy){
+    let i = 1;
+    newKey = key + "_" + i;
+  }
+
+  objectCopy[newKey] = value;
+
   return objectCopy;
 }
 
 function getObjectElements(keys) {
   let result = [];
-  // TODO: implement me
+
+  for(let i = 0; i < keys.length; i++){
+    if(keys[i] in object){
+      result[i] = object[keys[i]];
+    }else{
+      result[i] = "not found";
+    }
+  }
+
   return result;
 }
 
 function deleteObjectElements(keys) {
   let objectCopy = Object.assign({}, object)
   // TODO: implement me
+
+  for (let i = 0; i < keys.length; i++){
+    if(keys[i] in objectCopy){
+      // TODO: Delete
+      delete objectCopy[keys[i]]
+    }
+  }
   return objectCopy;
 }
