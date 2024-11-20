08eb15c8ed921e51c88107470bf65ddca6dd3aa5
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-20 17:39:02 +0100

    Solve Task 2.1.2 (addArrayElement() & getArrayElements())

--- ha02/js/a02.js
+++ ha02/js/a02.js
@@ -2,20 +2,54 @@ let array = ["hund", "katze", "maus", "elefant", "schlange", "stachelschwein", "
 
 function addArrayElement(element) {
   let arrayCopy = [...array];
-  // TODO: implement me
+  if (arrayCopy.includes(element)){
+    return arrayCopy;
+  }
+  arrayCopy.push(element);
   return arrayCopy;
 }
 
 function getArrayElements(number, startIndex) {
   let result = [];
-  // TODO: implement me
+  if (!array || array.length === 0 || number <= 0){
+    return result;
+  }
+
+  let normalizedIndex = startIndex % array.length
+
+  let i = normalizedIndex;
+  while (i < array.length && i < normalizedIndex + number){
+    result.push(array[i]);
+    i++;
+  }
+
   return result;
+
 }
 
 function deleteArrayElements(number, startIndex, everyIth) {
-  let arrayCopy = [...array];
-  let removedItems = [];
-  // TODO: implement me
+  let arrayCopy = []
+  let removedItems = []
+  if(!array || array.length === 0 || number <= 0){
+    return {
+      newResult: arrayCopy,
+      removedItems: removedItems
+    }
+  }
+
+  let normalizedIndex = startIndex % (array.length - 1)
+
+  let i = normalizedIndex;
+  while (i < array.length && i < (normalizedIndex + number)){
+    arrayCopy.push(array[i]);
+    i++;
+  }
+
+  for(let i = 0; i < arrayCopy.length; i += everyIth){
+    removedItems.push(arrayCopy[i])
+    arrayCopy[i] = null
+  }
+
   return {
     newResult: arrayCopy,
     removedItems: removedItems
