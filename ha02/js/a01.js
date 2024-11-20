faeb45db6fc9bf705288d3a5edd5034a9927d8a3
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-20 17:38:21 +0100

    Refactor multiplyAll()

--- ha02/js/a01.js
+++ ha02/js/a01.js
@@ -8,8 +8,8 @@ function multiply(number1, number2) {
 
 function multiplyAll() {
   // TODO: implement me
-  var result = 1;
-  for (var i = 0; i < arguments.length; i++){
+  let result = 1;
+  for (let i = 0; i < arguments.length; i++){
     result *= arguments[i];
   }
   return result;
