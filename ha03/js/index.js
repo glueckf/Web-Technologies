d8d5891c2343b1791139e4aae354eafbd695f496
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-12-05 17:17:47 +0100

    Add HA03

---
+++ ha03/js/index.js
@@ -0,0 +1,24 @@
+
+// Aufgabe 3.1.1
+function notGoodGrades(grades) {
+  return grades
+    // TODO: implement me
+}
+
+// Aufgabe 3.1.2
+function gradeOverview(students, grades) {
+  return students
+    // TODO: implement me
+}
+
+// Aufgabe 3.1.3
+function duplicateStudents(students) {
+  return students
+    // TODO: implement me
+}
+
+// Aufgabe 3.1.4
+function invalidGrades(grades) {
+  return grades
+    // TODO: implement me
+}
