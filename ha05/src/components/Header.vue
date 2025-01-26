ff2b1fd07f310209bb5d35bdd2a3d295e7eead74
Finn Glück <f.glueck@campus.tu-berlin.de>
2025-01-26 10:14:57 +0100

    feat: create Header component with logo and title

---
+++ ha05/src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import logo from '@/assets/logo.svg'
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <nav class="navbar bg-light">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0">
+        <img :src="logo" alt="Vue Logo" height="24" class="d-inline-block align-text-top"/>
+        {{ title }}
+      </span>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+</style>
