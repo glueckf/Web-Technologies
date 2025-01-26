4e023da7adb0b20ef375e0f51f4d74af4e3d77b7
Finn Glück <f.glueck@campus.tu-berlin.de>
2025-01-26 10:27:47 +0100

    feat: add basic Column component with props

---
+++ ha05/src/components/Column.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  column: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h4> ({column.name}) </h4>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
