4e766185bd8ff522acf8ae64727c13737c43cd72
Finn Glück <f.glueck@campus.tu-berlin.de>
2025-01-26 10:22:58 +0100

    feat: create basic Board component structure
    - Add component setup with columns prop
    - Create basic grid layout
    - Implement column iteration

---
+++ ha05/src/components/Board.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Column from './Column.vue'
+
+defineProps({
+  columns: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div v-for="column in columns"
+           :key="column.id"
+           class="col">
+        <Column :column="column" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
